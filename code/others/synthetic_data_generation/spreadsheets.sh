b48c9407cce2f1ca093e3d1ac11f5c7f3eb49313
Flavio Hafner <f.hafner@esciencecenter.nl>
2024-04-26 13:55:09 +0200

    add bash script for spreadsheets

---
+++ code/others/synthetic_data_generation/spreadsheets.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#SBATCH --job-name=spreadsheets
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --mem=0
+#SBATCH -p work_env
+#SBATCH -e /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/synthetic/logs/generate_stderr.txt
+#SBATCH -o /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/synthetic/logs/generate_stdout.txt
+
+module purge 
+module load 2022 
+module load Python/3.10.4-GCCcore-11.3.0
+module load SciPy-bundle/2022.05-foss-2022a
+module load matplotlib/3.5.2-foss-2022a
+source /gpfs/ostor/ossc9424/homedir/ossc_new/bin/activate 
+
+DATAPATH="/gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/"
+
+echo "job started" 
+cd /gpfs/ostor/ossc9424/homedir/synthetic_data/
+
+date
+time python gen_spreadsheets.py "$DATAPATH/dutch_real/raw_data/" "$DATAPATH/synthetic/raw_data/"
+
+echo "job ended successfully" 

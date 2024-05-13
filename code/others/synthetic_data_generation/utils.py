d7af815ea4eed6d3b340082c9730d85a671802ba
Flavio Hafner <f.hafner@esciencecenter.nl>
2024-05-13 12:54:15 +0200

    fix bug with dropping unnamed column

--- code/others/synthetic_data_generation/utils.py
+++ code/others/synthetic_data_generation/utils.py
@@ -72,7 +72,8 @@ def sample_from_file(source_file_path, n_rows):
   else:
     raise ValueError(f"wrong file extension found for {source_file_path}")
   
-  df.drop(columns=["Unnamed: 0"], inplace=True)
+  if "Unnamed: 0" in df.columns:
+    df.drop(columns=["Unnamed: 0"], inplace=True)
 
   return df, nobs
 

5c27e5af24f85428631590fd675b70ee09801054
Flavio Hafner <f.hafner@esciencecenter.nl>
2024-05-24 14:23:17 +0200

    update conf and slurm scripts

--- code/llm/slurm_scripts/pretrain_medium.sh
+++ code/llm/slurm_scripts/pretrain_medium.sh
@@ -4,15 +4,26 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
 #SBATCH --time=100:00:00
-#SBATCH --mem=100G
+#SBATCH --mem=80G
 #SBATCH -p comp_env
-#SBATCH --gres=gpu:1
-#SBATCH -e /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/dutch_real/logs/pretrain_medium_stderr.txt
-#SBATCH -o /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/dutch_real/logs/pretrain_medium_stdout.txt
-
+#SBATCH -e /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/dutch_real/logs/%x.%j.err
+#SBATCH -o /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/dutch_real/logs/%x.%j.out
 
 echo "job started"
+declare PREFIX="/gpfs/ostor/ossc9424/homedir/"
+declare VENV="ossc_new"
 
+module purge 
+module load 2022 
+module load Python/3.10.4-GCCCore-11.3.0
+module load PyTorch/1.12.0-foss-CUDA-11.7.0
+module load SciPy-bundle/2022.05-foss-2022a
+module load matplotlib/3.5.2-foss-2022a
+source "$HOMEDIR"/"$VENV"/bin/activate
+
+export CUDA_VISIBLE_DEVICES=1
+
+cd "$PREFIX"/Tanzir/LifeToVec_Nov/
 
 date
 time python pretrain.py projects/dutch_real/pretrain_cfg_medium.json

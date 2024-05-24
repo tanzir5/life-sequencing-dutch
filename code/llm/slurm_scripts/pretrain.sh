e5afcc3b419479961164edfc59e02238f960f5bd
tanzir5 <tanzir2430@gmail.com>
2024-05-24 00:47:36 -0400

    fixes pretrain.sh

--- code/llm/slurm_scripts/pretrain.sh
+++ code/llm/slurm_scripts/pretrain.sh
@@ -3,13 +3,15 @@
 #SBATCH --job-name=pretrain
 #SBATCH --ntasks-per-node=1
 #SBATCH --nodes=1
-#SBATCH --time=03:00:00
-#SBATCH --mem=180G
+#SBATCH --time=100:00:00
+#SBATCH --mem=100G
 #SBATCH -p comp_env
-#SBATCH -e /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/dutch_real/logs/pretrain_stderr.txt
-#SBATCH -o /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/dutch_real/logs/pretrain_stdout.txt
+#SBATCH --gres=gpu:1
+#SBATCH -e /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/dutch_real/logs/pretrain_small_stderr.txt
+#SBATCH -o /gpfs/ostor/ossc9424/homedir/Tanzir/LifeToVec_Nov/projects/dutch_real/logs/pretrain_small_stdout.txt
 
 echo "job started"
+
 
 date
 time python pretrain.py projects/dutch_real/pretrain_cfg_small.json

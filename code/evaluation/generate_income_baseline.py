84deea66619bdff3136d41995fe84c567ded4076
dakota0064 <dakota.handzlik@stonybrook.edu>
2024-06-28 10:09:46 -0400

    Removed user set check, now get data for all possible users

--- code/evaluation/generate_income_baseline.py
+++ code/evaluation/generate_income_baseline.py
@@ -2,15 +2,6 @@ import pandas as pd
 import pyreadstat
 import pickle
 
-# Get RINPERSONs of people who we want to check for income
-df = pd.read_csv("RINPERSOON_and_income_30.txt",
-                 delimiter="\t",
-                 usecols=["RINPERSOON"],
-                 dtype={'RINPERSOON': int, 'incomeAge30': int, 'birthYear': int})
-
-user_set = set(df['RINPERSOON'])
-print(len(user_set), flush=True)
-
 baseline_by_years = {}
 years = ['2011', '2012', '2013', '2014', '2015', '2016',
          '2017', '2018', '2019', '2020', '2021', '2022']
@@ -35,8 +26,7 @@ for year in years:
         except:
             continue
 
-        if user in user_set:
-            yearly_baseline[user] = income
+        yearly_baseline[user] = income
 
     baseline_by_years[year] = yearly_baseline
 
